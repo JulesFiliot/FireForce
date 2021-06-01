f09706b5c3eef79da8c40147392013cf603eaa68
Matthieu Gourdon <matthieu.gourdon@tpg02.cpe.fr>
2021-06-01 15:23:01 +0200

    exting fire vehic

---
+++ backend/exting/src/main/java/com/fireforce/exting/ExtingApplication.java
@@ -0,0 +1,13 @@
+package com.fireforce.exting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtingApplication.class, args);
+	}
+
+}
---
+++ backend/exting/src/test/java/com/fireforce/exting/ExtingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fireforce.exting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
