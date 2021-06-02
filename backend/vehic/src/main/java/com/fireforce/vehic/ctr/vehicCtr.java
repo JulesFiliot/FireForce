bf84e95505eebb7e2236a8a26c392b659af876aa
PierreCruveiller <pierre.cruveiller@cpe.fr>
2021-06-02 14:55:53 +0200

    vehicle creation in Simulator through vehic microservice (edition soon)

--- backend/vehic/src/main/java/com/fireforce/vehic/ctr/vehicCtr.java
+++ backend/vehic/src/main/java/com/fireforce/vehic/ctr/vehicCtr.java
@@ -1,14 +1,17 @@
 package com.fireforce.vehic.ctr;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.fireforce.vehic.model.vehic;
 import com.fireforce.vehic.serv.vehicServ;
+import com.project.model.dto.VehicleDto;
 
 @RestController
 public class vehicCtr {
@@ -16,9 +19,14 @@ public class vehicCtr {
 	@Autowired
 	vehicServ vServ;
 	
-	@RequestMapping(method = RequestMethod.POST,value = "/addVehic")
+	@RequestMapping(method = RequestMethod.POST,value = "/vehicle")
 	public void addVehic(@RequestBody vehic v) {
 		vServ.addVehic(v);
+		VehicleDto t = new VehicleDto(v.getId().intValue(),v.getLon(),v.getLat(),v.getType(),v.getEfficiency(),v.getLiquidType(),v.getLiquidQuantity(),v.getLiquidConsumption(),v.getFuel(),v.getFuelConsumption(),v.getCrewMember(),v.getCrewMemberCapacity(),v.getFacilityRefID().intValue());
+		
+    	String reqUrl = "http://127.0.0.1:8081/vehicle";
+        RestTemplate restTemplate = new RestTemplate();
+		restTemplate.postForEntity(reqUrl, t,Object.class);
 	}
 	
 	@RequestMapping(value = "/generateVehics")
@@ -35,4 +43,5 @@ public class vehicCtr {
 	public void newCoord(@PathVariable Integer id, @PathVariable double lat, @PathVariable double lon) {
 		vServ.newCoord(vServ.getVehic(id), lat, lon);
 	}
+
 }
