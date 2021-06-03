46301b3c93261400e7f07b84963fc290c2a2face
MatthieuGourdon <matthieu.gourdon@cpe.fr>
2021-06-03 09:29:50 +0200

    fire init

--- backend/fire/src/main/java/com/fireforce/fire/ctr/fireCtr.java
+++ backend/fire/src/main/java/com/fireforce/fire/ctr/fireCtr.java
@@ -1,8 +1,12 @@
 package com.fireforce.fire.ctr;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fireforce.fire.model.fire;
 import com.fireforce.fire.serv.fireServ;
 
 @RestController
@@ -10,4 +14,9 @@ public class fireCtr {
 	
 	@Autowired
 	fireServ fServ;
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/fire")
+	public void addVehic() {
+		fServ.addFire();
+	}
 }
