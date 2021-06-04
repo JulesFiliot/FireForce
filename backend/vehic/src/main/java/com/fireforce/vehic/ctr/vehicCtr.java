d67f9b8a8b06407543dc3582df30afbdef751869
Matthieu Gourdon <matthieu.gourdon@tpc30.cpe.fr>
2021-06-04 17:47:08 +0200

    getAllFire for front

--- backend/vehic/src/main/java/com/fireforce/vehic/ctr/vehicCtr.java
+++ backend/vehic/src/main/java/com/fireforce/vehic/ctr/vehicCtr.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,7 +37,8 @@ public class vehicCtr {
 		vServ.moveVehic(c,id);
 	}
 	
-	@RequestMapping(value = "/getAllVehic")
+	@CrossOrigin
+	@RequestMapping(value = "/getAllVehic") //Si ca marche pas cf dto et non repo
 	public ArrayList<vehic> getAllVehic() {
     	ArrayList<vehic> vList = vServ.getAllVehic();
     	return vList;
